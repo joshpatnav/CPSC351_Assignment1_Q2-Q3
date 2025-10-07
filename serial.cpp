50354eadf1a2427289b203266f1bb4f8d6925700
joshpatnav <joshpatnav@csu.fullerton.edu>
2025-10-07 12:03:38 -0700

    Finished serial.cpp?

--- serial.cpp
+++ serial.cpp
@@ -3,4 +3,72 @@ Serial Fetcher
 */
 
 #include <iostream>
-#include <fstream>
+#include <fstream> // open(), close()
+#include <sstream> //istring, ostring
+#include <string> // strings
+#include <unistd.h> // fork, execlp
+#include <sys/wait.h> // wait
+
+using namespace std;
+
+int main() {
+
+  ifstream infile("input.txt");
+  if (!infile) {
+    cerr << "Error: Cant open input.txt\n";
+    return 0;
+  }
+
+  string line;
+  int count = 1;
+
+  cout << "Parent process started\n";
+  cout << "Parent PID: " << getpid()
+      << ", UID: " << getuid()
+      << ", GID: " << getgid() << "\n\n";
+
+  while (getline(infile, line))
+  {
+    istringstream iss(line);
+    double lat, lon;
+    if (!(iss >> lat >> lon))
+      continue; // skip invalid lines
+
+    pid_t pid = fork();
+
+    if (pid < 0)
+    {
+      cerr << "Error: fork failed\n";
+      return 1;
+    }
+    else if (pid == 0)
+    {
+      // Child process
+      cout << "Child " << count << " created.\n";
+      cout << " PID: " << getpid()
+           << ", PPID: " << getppid()
+           << ", UID: " << getuid()
+           << ", GID: " << getgid() << "\n";
+
+      string filename = "file" + to_string(count) + ".json";
+      string url = "https://api.open-meteo.com/v1/forecast?latitude=" +
+                      to_string(lat) + "&longitude=" +
+                      to_string(lon) + "&current_weather=True";
+
+      execlp("/usr/bin/curl", "curl", "-s", "-o", filename.c_str(),
+              url.c_str(), (char *)NULL);
+      perror("execlp failed");
+      _exit(1);
+    }
+    else
+    {
+      // Parent waits for this child
+      int status;
+      waitpid(pid, &status, 0);
+      cout << "Fetched city " << count << " (lat=" << lat << ", lon=" << lon << ")\n\n";
+      count++;
+    }
+  }
+  cout << "All weather files downloaded\n";
+  return 0;
+}
